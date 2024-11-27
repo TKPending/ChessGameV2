ae66b8c546666f69b4abcdf4d065567bfca06a47
Tony K. <tony-koke@outlook.com>
2024-11-27 11:20:27 +0000

    Display Move Section

--- src/app/containers/chessboard/PreviousMovesContainer.tsx
+++ src/app/containers/chessboard/PreviousMovesContainer.tsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 const PreviousMovesContainer = () => {
-    return (
-      <div className="h-[90%] w-[20%] max-w-sm bg-gray-900 rounded-md p-4 overflow-auto">
-        <p className="text-white text-center">Previous Moves</p>
-      </div>
-    );
+  const [previousMovesHidden, setPreviousMovesHidden] = useState(false);
+
+  const togglePreviousMoves = () => {
+    setPreviousMovesHidden(!previousMovesHidden);
   };
-  
-  export default PreviousMovesContainer;
-  
+
+  return (
+    <div className="h-[90%] w-[20%] max-w-sm rounded-md p-4 overflow-auto">
+      <p
+        onClick={togglePreviousMoves}
+        className={`text-black text-center bg-white text-xl p-4 ${!previousMovesHidden ? "rounded-tr-lg rounded-tl-lg" : "rounded-lg"} hover:cursor-pointer`}
+      >
+        {previousMovesHidden ? "Display Moves" : "Hide Moves"}
+      </p>
+
+      <div className={`${!previousMovesHidden && "h-full w-full bg-white"} rounded-br-lg rounded-bl-lg`}></div>
+    </div>
+  );
+};
+
+export default PreviousMovesContainer;
