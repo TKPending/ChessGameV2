f68504bc14f3b1a3305bbdd4ec202f67dc005913
TKPending <tony-koke@outlook.com>
2025-12-05 02:24:59 +0000

    CG11 Deployment Fixes

--- src/app/containers/chessboardMoveHistory/ChessboardMoveHistory.tsx
+++ src/app/containers/chessboardMoveHistory/ChessboardMoveHistory.tsx
@@ -1,11 +1,11 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import ChessMovesHeader from "./component/ChessMovesHeader";
 import ChessMovesContainer from "./containers/ChessMovesContainer";
 import ResetGame from "@/app/containers/features/resetGame/ResetGame";
-import { selectCurrentMoveCount } from "@/app/utils/selectors/chessboardHistoryStateSelector";
+// import { selectCurrentMoveCount } from "@/app/utils/selectors/chessboardHistoryStateSelector";
 
 const ChessboardMoveHistory = () => {
-  const currentMoveCount: number = useSelector(selectCurrentMoveCount);
+  // const currentMoveCount: number = useSelector(selectCurrentMoveCount);
 
   return (
     <div className="min-h-[20px] h-[120px] md:h-full p-2 w-full md:max-w-[300px] mt-5 flex flex-col items-center justify-center">
