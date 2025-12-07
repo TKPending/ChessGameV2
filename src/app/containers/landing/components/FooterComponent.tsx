f7a9d8bc60b50c1978a211ef227cffcc79c4eac2
TKPending <tony-koke@outlook.com>
2025-12-07 23:11:28 +0000

    CG14.1 Landing Page Sizing Update

--- src/app/containers/landing/components/FooterComponent.tsx
+++ src/app/containers/landing/components/FooterComponent.tsx
@@ -3,19 +3,15 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const FooterComponent = () => {
+  const className: string =
+    "animate-from-right text-customGreen h-8 w-8 hover:opacity-80 transition duration-200";
   return (
-    <div className="flex gap-4 absolute items-center right-[10%] bottom-[10%] hover:cursor-pointer z-50">
+    <div className="flex gap-4 absolute items-center right-[10%] bottom-[5%] hover:cursor-pointer z-50">
       <a href={"https://github.com/TKPending/ChessGameV2"} target={"_blank"}>
-        <FontAwesomeIcon
-          icon={faGithub}
-          className="animate-from-right text-customGreen h-6 w-6"
-        />
+        <FontAwesomeIcon icon={faGithub} className={className} />
       </a>
       <a href={`mailto:tony-koke@outlook.com`}>
-        <FontAwesomeIcon
-          icon={faEnvelope}
-          className="animate-from-right text-customGreen h-6 w-6"
-        />
+        <FontAwesomeIcon icon={faEnvelope} className={className} />
       </a>
     </div>
   );
