26c0aae3926dc356e585a1e76f469500e4d2d70e
TKPending <tony-koke@outlook.com>
2025-12-09 22:30:00 +0000

    CG15 Modal Refactor

--- src/app/containers/pawnPromotion/components/PawnPromotionComponent.tsx
+++ src/app/containers/pawnPromotion/components/PawnPromotionComponent.tsx
@@ -1,7 +1,8 @@
 import { useSelector } from "react-redux";
-import { ChessColors, PieceName } from "@/app/types/ChessTypes";
+import ModalTemplate from "@/app/containers/modal/ModalTemplate";
 import { selectCurrentTurn } from "@/app/utils/selectors/gameStateSelectors";
 import { getPlayerColor } from "@/app/utils/getPlayerColor";
+import { ChessColors, PieceName } from "@/app/types/ChessTypes";
 
 type PawnPromotionComponentProps = {
   handlePiecePromotion: (pieceName: PieceName) => void;
@@ -13,6 +14,13 @@ interface PromotionOptionType {
   alt: string;
 }
 
+/**
+ * Renders the modal for pawn promotion
+ * @param {PieceName} name Name of the piece being promoted
+ * @param {string} src URL to the image to render
+ * @param {string} alt alternate text for the image
+ * @returns Pawn Promotion Modal
+ */
 const PawnPromotionComponent = ({
   handlePiecePromotion,
 }: PawnPromotionComponentProps) => {
@@ -43,45 +51,26 @@ const PawnPromotionComponent = ({
   ];
 
   return (
-    <div
-      className={`fixed inset-0 z-50 flex items-center justify-center bg-black/80 backdrop-blur-sm`}
-    >
-      <div
-        className="absolute inset-0 pointer-events-none 
-        bg-[radial-gradient(circle_at_center,rgba(255,215,128,0.12),transparent_70%)]"
-      />
+    <ModalTemplate>
+      <p className="text-white font-semibold text-xl">Promote your pawn</p>
 
-      <div
-        className="
-        relative w-[500px] max-w-[92%]
-        bg-[rgba(12,12,14,0.86)]
-        backdrop-blur-xl
-        border border-[rgba(255,215,128,0.35)]
-        rounded-3xl shadow-[0_10px_45px_rgba(0,0,0,0.7)]
-        p-12 flex flex-col items-center gap-10
-        animate-premiumPop
-        "
-      >
-        <p className="text-white font-semibold text-xl">Promote your pawn</p>
-
-        <div className="flex h-auto w-auto flex items-center justify-center">
-          {promotionOptions.map((piece, index) => (
-            <div
-              key={index}
-              className="flex items-center justify-center h-16 w-20 hover:cursor-pointer duration-400 p-4 hover:bg-customGreen/80 
+      <div className="flex h-auto w-auto flex items-center justify-center">
+        {promotionOptions.map((piece, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-center h-16 w-20 hover:cursor-pointer duration-400 p-4 hover:bg-customGreen/80 
             text-white hover:shadow-[0_0_20px_rgba(50,200,120,0.6)] transition-all w-auto rounded-lg"
-              onClick={() => handlePiecePromotion(piece.name)}
-            >
-              <img
-                src={`${pieceColor}${piece.src}.png`}
-                alt={piece.alt}
-                className="w-12 h-12"
-              />
-            </div>
-          ))}
-        </div>
+            onClick={() => handlePiecePromotion(piece.name)}
+          >
+            <img
+              src={`${pieceColor}${piece.src}.png`}
+              alt={piece.alt}
+              className="w-12 h-12"
+            />
+          </div>
+        ))}
       </div>
-    </div>
+    </ModalTemplate>
   );
 };
 
